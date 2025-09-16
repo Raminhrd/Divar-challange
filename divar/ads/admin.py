8bdc5641269b8e095244a9321ad2280b471e0d9e
raminhrd <raminhr.work@gmail.com>
2025-09-16 22:54:58 +0330

    add_admin

--- divar/ads/admin.py
+++ divar/ads/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from ads.models import *
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'description']
+    search_fields = ['title']
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+
+class AdsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'description', 'price', 'city', 'created_at']
+    search_fields = ['title']
+    list_filter = ['city', 'category']
+
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Ad, AdsAdmin)
